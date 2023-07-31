fe2207dbcf6c1827a2fe97dbd96a751d38447f8b
gitgunny <w3aries@naver.com>
2023-08-01 04:13:53 +0900

    Up & Down

--- hello_qt/hello_qt.cpp
+++ hello_qt/hello_qt.cpp
@@ -1,10 +1,36 @@
 #include "hello_qt.h"
 
-hello_qt::hello_qt(QWidget* parent)
-	: QMainWindow(parent)
+hello_qt::hello_qt(QWidget* parent) : QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	srand(time(NULL));
+	answer = rand() % 100 + 1;
+
+	ui.lineEdit->setPlaceholderText("1 ~ 100 Input");
 }
 
 hello_qt::~hello_qt()
 {}
+
+void hello_qt::checkAnswer()
+{
+	QMessageBox msg_box;
+
+	bool check_num;
+	unsigned int input_num = ui.lineEdit->text().toInt(&check_num);
+
+	if (check_num)
+	{
+		if (input_num < 1 || input_num > 100)
+			msg_box.warning(nullptr, nullptr, "1 ~ 100 Input");
+		else if (input_num < answer)
+			msg_box.information(nullptr, nullptr, "Up");
+		else if (input_num > answer)
+			msg_box.information(nullptr, nullptr, "Down");
+		else
+			msg_box.information(nullptr, nullptr, "Collect!");
+	}
+	else
+		msg_box.warning(nullptr, nullptr, "Is not Number");
+}
--- hello_qt/hello_qt.h
+++ hello_qt/hello_qt.h
@@ -1,7 +1,12 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <qmessagebox.h>
 #include "ui_hello_qt.h"
+
+#include <cstdlib>
+#include <string>
+#include <time.h>
 
 class hello_qt : public QMainWindow
 {
@@ -13,4 +18,10 @@ public:
 
 private:
 	Ui::hello_qtClass ui;
+
+public:
+	int answer;
+
+public slots:
+	void checkAnswer();
 };
